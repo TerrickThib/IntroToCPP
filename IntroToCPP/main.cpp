6d927d774f6209255702fba632c7b0f98247bd32
Terrick Thibodeaux <terrickthibodeaux@gmail.com>
2021-11-29 16:56:21 -0600

    You Welcom C++

---
+++ IntroToCPP/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+#include "Entity.h"
+
+int main()
+{
+	Entity wompus = Entity('W', 1000000000, 5, -20);
+
+	//Waits for player input
+	system("pause");
+
+	//Clears players screen
+	system("cls");
+	return 0;
+}
